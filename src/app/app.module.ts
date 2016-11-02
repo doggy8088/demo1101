3eecee806f3e3a6cbcbcde0950a990d4587de0d5
Will <doggy.huang@gmail.com>
2016-11-02 18:49:27 +0800

    建立 BadwordsPipe 元件 ( ng g pipe badwords )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BadwordsPipe } from './badwords.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BadwordsPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/badwords.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BadwordsPipe } from './badwords.pipe';
+
+describe('Pipe: Badwords', () => {
+  it('create an instance', () => {
+    let pipe = new BadwordsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
